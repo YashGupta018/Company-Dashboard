87cff6dd91c7a1c21fb881632456cf522d9c8aaa
Yash Gupta <ygupta2001@gmail.com>
2023-07-18 19:04:28 +0530

    Sidebar & logo added

--- chaabi/src/App.js
+++ chaabi/src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logoImage from './assets/logo.svg'; // Replace 'logo.svg' with the actual path to your logo image
 
 function App() {
   return (
     <div className="App">
+      <div className="Container">
+        <div className="Logo">
+          <img src={logoImage} alt="Logo" className="LogoImage" />
+        </div>
+        <div className="Separator"></div>
+      </div>
+      <div className="Sidebar">
+        {/* Sidebar content */}
+      </div>
+      <div className="MainPage">
+        {/* Main page content */}
+      </div>
     </div>
   );
 }
